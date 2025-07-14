06b9b811066aa74d4b100ffe8e2915fe2914a4e6
K-Michallik <91637208+K-Michallik@users.noreply.github.com>
2025-07-14 14:08:05 +0000

    update to Angular 19; change functionality to accessing Wired IO via frontend.

--- src/app/components/access-services/access-services.component.ts
+++ src/app/components/access-services/access-services.component.ts
@@ -1,15 +1,17 @@
 import { TranslateService } from '@ngx-translate/core';
-import { first } from 'rxjs/operators';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { ApplicationPresenterAPI, ApplicationPresenter, RobotSettings, PayloadLimits } from '@universal-robots/contribution-api';
+import { first, take } from 'rxjs/operators';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { ApplicationPresenterAPI, ApplicationPresenter, RobotSettings, PayloadLimits, Signal, SignalBooleanValue, SignalEvent } from '@universal-robots/contribution-api';
 import { AccessServicesNode } from './access-services.node';
+import { Subscription } from 'rxjs';
 
 @Component({
     templateUrl: './access-services.component.html',
     styleUrls: ['./access-services.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: false
 })
-export class AccessServicesComponent implements ApplicationPresenter, OnChanges {
+export class AccessServicesComponent implements ApplicationPresenter, OnChanges, OnDestroy {
     // applicationAPI is optional
     @Input() applicationAPI: ApplicationPresenterAPI;
     // robotSettings is optional
@@ -18,6 +20,8 @@ export class AccessServicesComponent implements ApplicationPresenter, OnChanges 
     @Input() applicationNode: AccessServicesNode;
 
     apiResponse: Promise<number> | null = null;
+
+    private sourceUpdatesSubscription?: Subscription
 
     constructor(
         protected readonly translateService: TranslateService,
@@ -48,10 +52,64 @@ export class AccessServicesComponent implements ApplicationPresenter, OnChanges 
     }
 
     onButtonPress(): void {
-        this.apiResponse =this.applicationAPI.safetyService.getPayloadLimits()
-        .then(
-            (response: PayloadLimits) => response.hardLimit.value
-        )
+    this.sourceUpdatesSubscription = this.applicationAPI.sourceService
+      .getSourceUpdates("ur-robot-io", "ur-tool-io")
+      .subscribe({
+        next: (event: SignalEvent) => {
+          console.log('Received SignalEvent:', event);
+        },
+        error: (err) => {
+          console.error('SourceUpdates Observable errored:', err);
+        },
+        complete: () => {
+          console.log('SourceUpdates Observable completed');
+        }
+      });
+    }
+
+    onDestroyPress(): void {
+        if (this.sourceUpdatesSubscription && !this.sourceUpdatesSubscription.closed) {
+            this.sourceUpdatesSubscription.unsubscribe();
+            console.log('Unsubscribed successfully.');
+        }
+        else {
+            console.log(`No active subscription.`);
+            
+        } 
+    }
+
+    async onWiredPress(): Promise<void> {
+      const val: SignalBooleanValue = {
+        signalID: 'DO 0',
+        type:     'signal_boolean_value',
+        value:    true
+      };
+      await this.applicationAPI.sourceService.setSourceSignalValue(
+        'ur-robot-io',
+        'ur-wired-io',
+        'DO 0',
+        val
+      );
+    }
+
+    async onToolPress(): Promise<void> {
+      const val: SignalBooleanValue = {
+        signalID: 'DO 0',
+        type:     'signal_boolean_value',
+        value:    true
+      };
+      await this.applicationAPI.sourceService.setSourceSignalValue(
+        'ur-robot-io',
+        'ur-tool-io',
+        'DO 0',
+        val
+      );
+    }
+
+
+    ngOnDestroy(): void {
+        console.log('ngOnDestroy was called.')
+        this.onDestroyPress();
     }
         
 
